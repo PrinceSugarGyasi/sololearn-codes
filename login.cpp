097db97efc7c3f92b0ffba06f49705d67e2fbda9
PrinceSugarGyasi <psgcecilia1960@gmail.com>
2023-04-25 02:59:14 +0000

    corrected mistake in copying

--- login.cpp
+++ login.cpp
@@ -2,22 +2,55 @@
 #include <string>
 using namespace std;
 
-class myClass {
-  public:
-    void setName(string x) {
-      name = x;
-    }
-    string getName() {
-      return name;
-    }
-  private:
-    string name;
+class userName{
+    private:
+        string name;
+    public:
+        void setName (string x){
+            name = x;
+        }
+        string getName(){
+            return name;
+        }
 };
 
-int main() {
-  myClass myObj;
-  myObj.setName("John");
-  cout << myObj.getName();
+class passWord{
+    private:
+        string password;
+    public:
+        void setLock (string y){
+            password = y;
+        }
+        string getLock(){
+                return password;
+        }
+};
 
-  return 0;
+
+int main(){
+    userName user;
+    passWord userp;
+    string username, pass;
+    cout << "USERNAME: ";
+    getline (cin, username);
+    cout << "PASSWORD: ";
+    getline (cin, pass);
+
+    user.setName (username);
+    userp.setLock (pass);
+
+    string loginName, loginLock;
+    cout << '\n' << "ENTER USERNAME TO LOGIN: ";
+    getline (cin, loginName);
+    cout << "ENTER PASSWORD TO LOGIN: ";
+    getline (cin, loginLock);
+
+
+    if (loginName == username && loginLock == pass){
+        cout << '\n' << "You successfully logged in, WELCOME!!!" << endl;;
+    } else {
+        cout << '\n' << "Login attempt failed, TRY AGAIN!!!" << endl;;
+    }
+
+    return 0;
 }
